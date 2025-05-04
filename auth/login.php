772cc8e9df2f9c208b55d4cd9eb7e5de67dc0caa
Auliarahmahsyahira-rpl1 <auliarahmahsyahira0@gmail.com>
2025-05-04 22:41:40 +0930

    menambahkan dokumentasi

--- auth/login.php
+++ auth/login.php
@@ -19,20 +19,17 @@ include(".layouts/header.php");
     <form class="mb-3" action="proses_login.php" method="POST">
       <!-- Judul form -->
       <h5 class="mb-2 text-center">Selamat datang! 👋</h5>
-
       <!-- Input username -->
       <div class="mb-3">
         <label class="form-label">Username</label>
         <input type="text" class="form-control" name="username"
           placeholder="Enter your username" autofocus required />
       </div>
-
       <!-- Input password -->
       <div class="mb-3 form-password-toggle">
         <div class="d-flex justify-content-between">
           <label class="form-label" for="password">Password</label>
         </div>
-
         <!-- Input dengan icon show/hide password -->
         <div class="input-group input-group-merge">
           <input type="password" class="form-control" name="password"
@@ -41,7 +38,6 @@ include(".layouts/header.php");
           <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
         </div>
       </div>
-
       <!-- Tombol submit -->
       <div class="mb-3">
         <button class="btn btn-primary d-grid w-100" type="submit">Masuk</button>
